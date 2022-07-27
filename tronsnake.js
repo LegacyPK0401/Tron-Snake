d22b020cd4149224002394c72ca9a0fd7fd1fd95
PK <kaptai1984@gmail.com>
2022-07-26 17:53:13 -0700

    Tried to get it to post

--- tronsnake.js
+++ tronsnake.js
@@ -3,10 +3,7 @@ class TronSnake {
     this.x = x
     this.y = y
     this.size = size
-    this.tail = [{
-      x: this.x,
-      y: this.y
-    }]
+    this.tail = [{x: this.x, y: this.y}]
     this.rotateX = 0
     this.rotateY = 1
 
@@ -16,22 +13,22 @@ class TronSnake {
     var newRect;
     if (this.rotateX == 1) {
       newRect = {
-        x: this.tail[this.tail.length - 1].x + this.size
+        x: this.tail[this.tail.length - 1].x + this.size,
         y: this.tail[this.tail.length - 1].y
       }
     } else if (this.rotateX == -1) {
       newRect = {
-        x: this.tail[this.tail.length - 1].x - this.size
+        x: this.tail[this.tail.length - 1].x - this.size,
         y: this.tail[this.tail.length - 1].y
       }
     } else if (this.rotateY == 1) {
       newRect = {
-        x: this.tail[this.tail.length - 1].x
+        x: this.tail[this.tail.length - 1].x,
         y: this.tail[this.tail.length - 1].y + this.size
       }
     } else if (this.rotateY == -1) {
       newRect = {
-        x: this.tail[this.tail.length - 1].x
+        x: this.tail[this.tail.length - 1].x,
         y: this.tail[this.tail.length - 1].y - this.size
       }
     }
@@ -44,36 +41,36 @@ class TronSnake {
 
 
 
-class Light(){
-  constructor(){
+class Light() {
+  constructor() {
     var isTouching;
-    while(true){
+    while (true) {
       isTouching = false;
-      this.x = Math.floor(Math.random() * canvas.width / lightcycle.size) * lightcycle.size
-      this.y = Math.floor(Math.random() * canvas.height / lightcycle.size) * lightcycle.size
-      for(var i = 0; i < lightcycle.tail.length;i++){
-        if(this.x == lightcycle.tail[i].x && this.y == lightcycle.tail.[i].y){
+      this.x = Math.floor(Math.random() * canvas.width / snake.size) * snake.size
+      this.y = Math.floor(Math.random() * canvas.height / snake.size) * snake.size
+      for (var i = 0; i < snake.tail.length; i++) {
+        if (this.x == snake.tail[i].x && this.y == snake.tail.[i].y) {
           isTouching = true
         }
       }
-      if(!isTouching){
+      if (!isTouching) {
         break;
       }
       this.color = "pink"
-      this.size = lightcycle.size
+      this.size = snake.size
     }
   }
 }
 
 var canvas = document.getElementById("canvas")
 
-var lightcycle = new TronSnake();
+var snake = new TronSnake(20,20,20);
 
-var idendisc = new Light();
+var apple = new Light();
 
 var canvasContext = canvas.getContext('2d');
 
-window.onload = () => {
+window.onload = ()=>{
   gameLoop();
 }
 
@@ -86,26 +83,51 @@ function show() {
   draw();
 }
 
-function update(){
+function update() {
+  canvasContext.clear(0,0 canvas.width, canvas.height)
+  console.log("update")
+  snake.move()
+  eatApple()
 
 }
 
-function draw(){
-  createRect(0,0,canvas.width, canvas.height, "black")
-  createRect(0,0, canvas.width, canvas.height)
-  for(var i =0; i < lightcycle.tail.length; i++){
-    createRect(lightcycle.tail[i].x + 2.5, lightcycle.tail[i].y + 2.5
-        lightcycle.size - 5, lightcycle.size - 5, 'white')
+functioneatApple(){
+  if(snake.tail[snake.tail.length -1].x)
+}
+
+function draw() {
+  createRect(0, 0, canvas.width, canvas.height, "black")
+  createRect(0, 0, canvas.width, canvas.height)
+  for (var i = 0; i < snake.tail.length; i++) {
+    createRect(snake.tail[i].x + 2.5, snake.tail[i].y + 2.5, snake.size - 5, snake.size - 5, 'white')
   }
 
   canvasContext.font = "20px Arial"
   canvasContext.fillStyle = "#00FF42"
-  canvasContext.fillText("Score: ",(lightcycle.tail.length +1),
-      canvas.width -120, 18);
-      createRect(idendisc.x, idendisc.y, idendisc.size, idendisc.size, idendisc,color)
+  canvasContext.fillText("Score: ", (snake.tail.length + 1),
+    canvas.width - 120, 18);
+  createRect(apple.x, apple.y, apple.size, apple.size, apple.color)
 }
 
 function createRect(x, y, width, height, color){
-    canvasContext.fillStyle = color
-    canvasContext.fillRect(x, y, width, height)
+  canvasContext.fillStyle = color
+  canvasContext.fillRect(x, y, width, height)
 }
+
+window.addEventListener("keydown", (event)=>{
+  setTimeout(()=>{
+    if(event.keyCode == 37 && sanke.rotateX != 1){
+      snake.rotateX = -1
+      snake.rotateY = 0;
+    } else if(event.keyCode == 38 && sanke.rotateY != 1){
+      snake.rotateX = 0
+      snake.rotateY = -1;
+    } else if(event.keyCode == 39 && sanke.rotateX != -1){
+      snake.rotateX = 1
+      snake.rotateY = 0;
+    } else if(event.keyCode == 40 && sanke.rotateY != -1){
+      snake.rotateX = 0
+      snake.rotateY = 1;
+    }
+  })
+})
