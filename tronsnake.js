53b8e5dbbdb352556690bd24a50a980c701ca461
PK <kaptai1984@gmail.com>
2022-08-03 19:23:03 -0700

    Finished Basic Tron Snake Game

--- tronsnake.js
+++ tronsnake.js
@@ -1,9 +1,9 @@
-class TronSnake {
+class TronCycle {
   constructor(x, y, size) {
     this.x = x
     this.y = y
     this.size = size
-    this.tail = [{x: this.x, y: this.y}]
+    this.tail = [{x:this.x, y:this.y}]
     this.rotateX = 0
     this.rotateY = 1
 
@@ -41,32 +41,37 @@ class TronSnake {
 
 
 
-class Light() {
-  constructor() {
+class IdenDisc{
+  constructor(){
+    console.log("light")
+    console.log(tron.size)
     var isTouching;
-    while (true) {
+    while(true){
       isTouching = false;
-      this.x = Math.floor(Math.random() * canvas.width / snake.size) * snake.size
-      this.y = Math.floor(Math.random() * canvas.height / snake.size) * snake.size
-      for (var i = 0; i < snake.tail.length; i++) {
-        if (this.x == snake.tail[i].x && this.y == snake.tail.[i].y) {
+      this.x = Math.floor(Math.random() * canvas.width / tron.size) * tron.size
+      this.y = Math.floor(Math.random() * canvas.height / tron.size) * tron.size
+      for (var i = 0; i < tron.tail.length; i++) {
+        if (this.x == tron.tail[i].x && this.y == tron.tail[i].y) {
           isTouching = true
         }
       }
+      console.log(this.x, this.y)
+      this.color = "orange"
+      this.size = tron.size
       if (!isTouching) {
         break;
       }
-      this.color = "pink"
-      this.size = snake.size
+      
+      
     }
   }
 }
 
 var canvas = document.getElementById("canvas")
 
-var snake = new TronSnake(20,20,20);
+var tron = new TronCycle(20,20,20);
 
-var apple = new Light();
+var light = new IdenDisc();
 
 var canvasContext = canvas.getContext('2d');
 
@@ -84,29 +89,46 @@ function show() {
 }
 
 function update() {
-  canvasContext.clear(0,0 canvas.width, canvas.height)
+  canvasContext.clearRect(0,0 , canvas.width, canvas.height)
   console.log("update")
-  snake.move()
-  eatApple()
-
+  tron.move()
+  eatIdenDisc()
+  checkHitWall()
 }
 
-functioneatApple(){
-  if(snake.tail[snake.tail.length -1].x)
+function checkHitWall(){
+  var headTail = tron.tail[tron.tail.length -1]
+  if(headTail.x == - tron.size) {
+    headTail.x = canvas.width - tron.size
+  } else if(headTail.x == canvas.width) {
+    headTail.x = 0
+  } else if(headTail.y == - tron.size) {
+    headTail.y = canvas.height - tron.size
+  } else if(headTail.y == canvas.height) {
+    headTail.y = 0
+  }
+}
+
+function eatIdenDisc(){
+  if(tron.tail[tron.tail.length - 1].x == light.x &&
+      tron.tail[tron.tail.length - 1].y == light.y){
+        tron.tail[tron.tail.length] = {x:light.x, y: light.y}
+        light = new IdenDisc();
+      }
 }
 
 function draw() {
   createRect(0, 0, canvas.width, canvas.height, "black")
   createRect(0, 0, canvas.width, canvas.height)
-  for (var i = 0; i < snake.tail.length; i++) {
-    createRect(snake.tail[i].x + 2.5, snake.tail[i].y + 2.5, snake.size - 5, snake.size - 5, 'white')
-  }
+  for (var i =0; i < tron.tail.length; i++) {
+    createRect(tron.tail[i].x + 2.5, tron.tail[i].y + 2.5, 
+      tron.size - 5, tron.size- 5, 'teal')
+    }
 
   canvasContext.font = "20px Arial"
   canvasContext.fillStyle = "#00FF42"
-  canvasContext.fillText("Score: ", (snake.tail.length + 1),
-    canvas.width - 120, 18);
-  createRect(apple.x, apple.y, apple.size, apple.size, apple.color)
+  canvasContext.fillText("Score: " + (tron.tail.length + 1), canvas.width - 120, 18);
+  createRect(light.x, light.y, light.size , light.size, light.color)
 }
 
 function createRect(x, y, width, height, color){
@@ -116,18 +138,18 @@ function createRect(x, y, width, height, color){
 
 window.addEventListener("keydown", (event)=>{
   setTimeout(()=>{
-    if(event.keyCode == 37 && sanke.rotateX != 1){
-      snake.rotateX = -1
-      snake.rotateY = 0;
-    } else if(event.keyCode == 38 && sanke.rotateY != 1){
-      snake.rotateX = 0
-      snake.rotateY = -1;
-    } else if(event.keyCode == 39 && sanke.rotateX != -1){
-      snake.rotateX = 1
-      snake.rotateY = 0;
-    } else if(event.keyCode == 40 && sanke.rotateY != -1){
-      snake.rotateX = 0
-      snake.rotateY = 1;
+    if(event.keyCode == 37 && tron.rotateX != 1){
+      tron.rotateX = -1
+      tron.rotateY = 0;
+    } else if(event.keyCode == 38 && tron.rotateY != 1){
+      tron.rotateX = 0
+      tron.rotateY = -1;
+    } else if(event.keyCode == 39 && tron.rotateX != -1){
+      tron.rotateX = 1
+      tron.rotateY = 0;
+    } else if(event.keyCode == 40 && tron.rotateY != -1){
+      tron.rotateX = 0
+      tron.rotateY = 1;
     }
   })
 })
